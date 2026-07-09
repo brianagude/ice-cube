5d1a86710c9df4948d5e1408fa4a774eb0fd3d95
Briana Gude <briana.gude@gmail.com>
2026-07-09 19:23:55 -0400

    wip pushing while tweaking my ice cube and floor physics

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import CANNON from "cannon";
+import * as CANNON from 'cannon-es'
 import * as dat from "lil-gui";
 import * as THREE from "three";
 import { RoundedBoxGeometry } from "three/addons/geometries/RoundedBoxGeometry.js";
@@ -13,6 +13,12 @@ const gui = new dat.GUI();
 
 const cubeVariables = {
 	size: 0.5,
+};
+
+const floorVariables = {
+  position: 0,
+	rotationX: 0.1,
+	rotationZ: 0.1,
 };
 
 /**
@@ -53,7 +59,8 @@ const camera = new THREE.PerspectiveCamera(
 	100,
 );
 
-camera.position.z = 5;
+camera.position.z = 7;
+camera.position.y = 3;
 scene.add(camera);
 
 // Controls
@@ -68,7 +75,6 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
 
 /**
  * Textures / Loaders
@@ -92,7 +98,6 @@ hdrLoader.load(
 	},
 );
 
-
 /**
  * Objects
  */
@@ -104,7 +109,7 @@ geometry.height = cubeVariables.size;
 geometry.depth = cubeVariables.size;
 geometry.segments = 1;
 geometry.radius = 0;
-console.log(geometry);
+// console.log(geometry);
 
 const material = new THREE.MeshPhysicalMaterial();
 material.roughness = 0.05;
@@ -113,43 +118,93 @@ material.reflectivity = 1;
 material.transmission = 1;
 material.thickness = 0.3;
 // material.map = texture                   // diffuse/color
-material.normalMap = normalTexture;         // fake surface bumps
+material.normalMap = normalTexture; // fake surface bumps
 // material.aoMap = occTexture              // ambient occlusion (contact shadows)
-material.roughnessMap = specTexture;        // often specularity ≈ inverse of roughness map
+material.roughnessMap = specTexture; // often specularity ≈ inverse of roughness map
 material.displacementMap = dispTexture;
 material.displacementScale = 0.05;
 material.attenuationColor = new THREE.Color(0xe5e6e2); // the tint light takes on as it travels through
-material.attenuationDistance = 0.5;         // how far light travels before fully absorbing that tint
+material.attenuationDistance = 0.5; // how far light travels before fully absorbing that tint
 
-console.log(material);
+// console.log(material);
 
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = 3
 scene.add(cube);
+
+// FLOOR
+const floor = new THREE.Mesh(
+	new THREE.BoxGeometry(10, 0.5, 10),
+	new THREE.MeshStandardMaterial({
+		color: "#777777",
+		metalness: 0.3,
+		roughness: 0.4,
+	}),
+);
+floor.receiveShadow = true;
+floor.position.y = floorVariables.position
+floor.rotation.x = Math.PI * floorVariables.rotationX
+floor.rotation.z = Math.PI * floorVariables.rotationZ
+scene.add(floor);
+console.log('floor', floor)
 
 /**
  * Physics
  */
-// const world = new CANNON.World();
-// world.gravity.set(0, -9.82, 0);
-// const cubeShape = new CANNON.Box(cubeVariables.size);
 
-// const cubeBody = new CANNON.Body({
-// 	mass: 1,
-// 	position: new CANNON.Vec3(0, 3, 0),
-// 	shape: cubeShape,
-// });
+// WORLD
+const world = new CANNON.World()
+world.gravity.set(0, - 9.82, 0)
 
-// world.addBody(cubeBody);
+// MATERIALS
+const defaultMaterial = new CANNON.Material('default')
 
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  {
+    friction: 0.1,
+  }
+)
 
+world.addContactMaterial(defaultContactMaterial)
+
+// PHYSICS CUBE
+const cubeSize = new CANNON.Vec3(cubeVariables.size, cubeVariables.size, cubeVariables.size)
+const cubeShape = new CANNON.Box(cubeSize)
+const cubeBody = new CANNON.Body({ mass: 1, shape: cubeShape, position: new CANNON.Vec3(0, 3, 0), material: defaultMaterial })
+world.addBody(cubeBody)
+
+// PHYSICS FLOOR
+const floorSize = new CANNON.Vec3(10, 0.5, 10)
+const floorShape = new CANNON.Box(floorSize)
+const floorBody = new CANNON.Body({ 
+  mass: 0, 
+  shape: floorShape, 
+  material: defaultMaterial, 
+  position: new CANNON.Vec3(0, floorVariables.position, 0) 
+})
+floorBody.quaternion.setFromEuler(
+  Math.PI * floorVariables.rotationX,
+  0,
+  Math.PI * floorVariables.rotationZ
+)
+world.addBody(floorBody)
 
 /**
  * Animation
  */
 const clock = new THREE.Clock();
+let oldElapsedTime = 0
 
 const tick = () => {
-	const elapsedTime = clock.getElapsedTime();
+	const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - oldElapsedTime
+  oldElapsedTime = elapsedTime
+
+  // Update physics
+  world.step(1 / 60, deltaTime, 3)
+  cube.position.copy(cubeBody.position)
+  cube.quaternion.copy(cubeBody.quaternion)
 
 	// Update controls
 	controls.update();
@@ -163,12 +218,12 @@ const tick = () => {
 
 tick();
 
-
 /**
  * Debugger Settings
  */
 const materialTweaks = gui.addFolder("Material");
 const cubeTweaks = gui.addFolder("Ice Cube");
+const floorTweaks = gui.addFolder("Floor");
 
 cubeTweaks
 	.add(cubeVariables, "size")
@@ -190,3 +245,33 @@ materialTweaks.add(material, "roughness").min(0).max(1).step(0.01);
 materialTweaks.add(material, "reflectivity").min(0).max(1).step(0.01);
 
 materialTweaks.add(material, "thickness").min(0).max(1).step(0.01);
+
+
+floorTweaks
+  .add(floor.rotation, "x")
+  .min(-0.4)
+  .max(0.4).step(0.01)
+  .onChange((value) => {
+    floorVariables.rotationX = value;
+    floorBody.quaternion.setFromEuler(
+      Math.PI * floorVariables.rotationX,
+      0,
+      Math.PI * floorVariables.rotationZ
+    )
+  });
+
+floorTweaks
+  .add(floor.rotation, "z")
+  .min(-0.4)
+  .max(0.4)
+  .step(0.01)
+  .onChange((value) => {
+		floorVariables.rotationZ = value;
+    floorBody.quaternion.setFromEuler(
+      Math.PI * floorVariables.rotationX,
+      0,
+      Math.PI * floorVariables.rotationZ
+    )
+	});;
+
+
